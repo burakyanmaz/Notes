6949860d41c9498f661771ae3dd57e7ea9d6dcbb
Yusuf U <hx0a1q@Yusufs-MacBook-Air.local>
2017-05-21 11:39:38 +0300

    max and min function example

--- Notes.playground/Contents.swift
+++ Notes.playground/Contents.swift
@@ -3,7 +3,7 @@
 import UIKit
 
 //MARK - Collection functions
-
+		
 /*
 - MAP
 */
@@ -18,3 +18,30 @@ let names = dictArray.map { $0["name"] as! String } // "["John", "Jack"]"
 
 //get ages
 let ageList = dictArray.map { $0["age"] as! Int}
+
+
+//mapping words
+let myWords = ["selam","Naber","MERHABA"].map { (string) -> String in
+	return string.uppercased()
+}
+let otherWords = ["naber", "gunaydın"].map { $0.uppercased() }
+
+let convertToInt = [1, 12.5, 127.12, 40.0, 5].map { Int($0) }
+
+
+// custom extension
+
+extension Array{
+	func customMap<T>(transform: (Element) -> T) -> [T] {
+		var list = [T]()
+		forEach { (list.append(transform ($0) )) }
+		return list
+	}
+}
+let productList = ["macbook", "ipad", "iphone"].customMap { "Apple" + $0 }
+
+
+//: MARK - max and min function
+
+
+
